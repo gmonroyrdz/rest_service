9b7aea80353a975bff5d7d5fe731e5ed34c0208d
gmonroyrdz <gmonroy@cua.uam.mx>
2025-04-11 15:14:53 -0600

    Agregué permisos de CORS

--- src/main/java/mx/uam/payroll/EmployeeController.java
+++ src/main/java/mx/uam/payroll/EmployeeController.java
@@ -2,6 +2,7 @@ package mx.uam.payroll;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // Permitir solicitudes desde el frontend
 public class EmployeeController {
     private final EmployeeRepository repository;
 
